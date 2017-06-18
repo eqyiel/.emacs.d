033bfa221a7b0450d7f7e19bc8c4e22113ad59e4
Ruben Maher <r@rkm.id.au>
2017-06-18 16:25:48 +0930

    Bunch of stuff

--- eqyi-el/eqyiel-custom-junk.el
+++ eqyi-el/eqyiel-custom-junk.el
@@ -3,9 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
+ '(org-agenda-files
    (quote
-    (mailcap yasnippet yaml-mode ws-butler web-mode visual-fill-column use-package tiny swift-mode sql-indent smex smartparens skewer-mode scss-mode rainbow-mode projectile pkgbuild-mode php-mode pass ox-gfm org-plus-contrib org-download org-caldav notmuch nodejs-repl nix-mode multiple-cursors matrix-client markdown-mode magit legalese key-chord json-mode highlight-indentation google-c-style gnorb flycheck flx expand-region eslint-fix emojify dtrt-indent counsel company-tern company-nixos-options company-emoji column-enforce-mode circe-notifications calfw buffer-move beacon bbdb-vcard auth-password-store))))
+    ("/home/eqyiel/org/emoji.org" "/home/eqyiel/org/org-caldav-inbox.org" "/home/eqyiel/org/phone-notes.org" "/home/eqyiel/org/shopping.org" "/home/eqyiel/org/todo-ayanami.org" "/home/eqyiel/org/todo-hanekawa.local.org" "/home/eqyiel/org/todo.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

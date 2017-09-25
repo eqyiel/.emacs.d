e8ac7cda6486ab00597bc807ec1b26b70c25ed15
Ruben Maher <r@rkm.id.au>
2017-09-25 20:44:47 +0930

    Don't error out for subtrees that already exist

--- scripts/pull-subtrees.sh
+++ scripts/pull-subtrees.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-
 git subtree pull --prefix site-lisp/nixos/nix-mode nixos/nix-mode master --squash
+git subtree pull --prefix site-lisp/travisbhartwell/nix-emacs travisbhartwell/nix-emacs master --squash
--- scripts/setup-remotes.sh
+++ scripts/setup-remotes.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-
-git remote add nixos/nix-mode git@github.com:NixOS/nix-mode.git
+git remote --verbose add nixos/nix-mode git@github.com:NixOS/nix-mode.git
+git remote --verbose add travisbhartwell/nix-emacs git@github.com:travisbhartwell/nix-emacs.git
